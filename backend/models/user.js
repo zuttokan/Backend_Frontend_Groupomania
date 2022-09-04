0f06f7ac3ad7a0e63a79d03732ddc3c372f16a3c
zuttokan <zutto.perez@gmail.com>
2022-09-04 03:12:33 +0100

    security

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,22 +1,6 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const { isEmail } = require('validator');
-//const { isStrongPassword } = require('validator');
-//const validator = require('validator');
-//let test = {minLength:6} as strongPasswordOptions
-//let options = {} as strongPasswordOptions;
-
-// const passwordValidator = require('password-validator');
-// let schemaPass = new passwordValidator();
-// schemaPass
-//   .is()
-//   .min(6) // Minimum length 6
-//   .is()
-//   .max(100) // Maximum length 100
-//   .has()
-//   .lowercase() // Must have lowercase letters
-//   .not()
-//   .spaces(); // Should not have spaces
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -29,15 +13,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    //validate: [isStrongPassword],
-    //validate: schemaPass,
     minLength: 6,
     maxLength: 100,
-    // validate(value) {
-    //   if (!validator.isStrongPassword(value)) {
-    //     throw new Error('Password is not Strong');
-    //   }
-    // },
   },
   isAdmin: { type: Boolean },
 });
